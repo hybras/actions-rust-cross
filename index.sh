2c3f882335ed47c2a90e17adc6a9674a6f03910c
hybras <24651269+hybras@users.noreply.github.com>
2024-05-24 17:13:36 -0400

    bash fu

--- index.sh
+++ index.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 TARGET=$1
 ARCH=$(cut -d'-' -f1 <<< "$TARGET")
 OTHER=$(cut -d'-' -f4 <<< "$TARGET")
 PACKAGE_ARCH="NOPE"
 TARGETS=$(rustc --print target-list)
+
+declare -A package_arch
+package_arch[arm]=arm
+package_arch[aarch64]=arm64
+package_arch[x86_64]=amd64
+package_arch[i686]=i386
+PACKAGE_ARCH=${package_arch[$ARCH]}
 
 if grep -xq "$TARGET" <<< "$TARGETS"; then
     echo "Target $TARGET is supported."
@@ -13,8 +22,13 @@ else
     exit 1
 fi
 
-case $ARCH in
-    arm)
+if [[ -z "$PACKAGE_ARCH" ]]; then
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+# handle arm subarchitectures
+if [[ "$ARCH" == "arm" ]]; then
     case $OTHER in
         gnueabihf | musleabihf)
         PACKAGE_ARCH="armhf"
@@ -27,27 +41,12 @@ case $ARCH in
         exit 1
         ;;
     esac
-    ;;
-    aarch64)
-    PACKAGE_ARCH="arm64"
-    ;;
-    x86_64)
-    PACKAGE_ARCH="amd64"
-    ;;
-    i686)
-    PACKAGE_ARCH="i386"
-    ;;
-    *)
-    echo "Unsupported architecture: $ARCH"
-    exit 1
-    ;;
-esac
+fi
 export DEBIAN_FRONTEND=noninteractive
 # pass package arch to github output
 echo "package_arch=$PACKAGE_ARCH" >> $GITHUB_OUTPUT
 if [[ -n "$CI" ]]; then
     sudo apt-get -y install crossbuild-essential-$PACKAGE_ARCH
+    mkdir -p ~/.cargo
+    cat cargo_config.toml >> ~/.cargo/config.toml
 fi
-
-mkdir -p ~/.cargo
-cat cargo_config.toml >> ~/.cargo/config.toml
